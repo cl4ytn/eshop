431bb6abfe57cde61f11a9d2ccb724ffa2f7b42d
cyll0 <c.nagle744@gmail.com>
2024-03-06 16:23:01 +0700

    [GREEN] Implement payment model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import enums.OrderStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Builder
+@Getter
+@Setter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
+    }
+}
